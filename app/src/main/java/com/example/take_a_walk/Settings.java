6e76749420fcd3fca38118b72c95b9fa4438dd74
kl4532 <kornellukasiak@gmail.com>
2020-10-02 13:33:05 +0200

    added validation to inputs

--- app/src/main/java/com/example/take_a_walk/Settings.java
+++ app/src/main/java/com/example/take_a_walk/Settings.java
@@ -4,23 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 
-public class Settings extends AppCompatActivity implements View.OnClickListener{
+public class Settings extends AppCompatActivity implements View.OnClickListener {
 
     Button btnStartCycle;
-    TextView workVal;
-    TextView walkVal;
+    EditText workVal;
+    EditText walkVal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        workVal = (TextView) findViewById(R.id.inputWork);
-        walkVal = (TextView) findViewById(R.id.inputWalk);
+        workVal = (EditText) findViewById(R.id.inputWork);
+        walkVal = (EditText) findViewById(R.id.inputWalk);
+
+//      Validate inputs
+        workVal.addTextChangedListener(new TextChangeListener<EditText>(workVal) {
+            @Override
+            public void onTextChanged(EditText target, Editable s) {
+                btnStartCycle.setEnabled(isValid(workVal));
+            }
+        });
+        walkVal.addTextChangedListener(new TextChangeListener<EditText>(walkVal) {
+            @Override
+            public void onTextChanged(EditText target, Editable s) {
+                btnStartCycle.setEnabled(isValid(walkVal));
+            }
+        });
 
         btnStartCycle = (Button) findViewById(R.id.btnStartCycle);
         btnStartCycle.setOnClickListener(Settings.this);
@@ -28,14 +43,20 @@ public class Settings extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     public void onClick(View v) {
-        setContentView(R.layout.activity_timeout);
         Intent timeoutActivity = new Intent(Settings.this, Timeout.class);
         timeoutActivity.putExtra("mode", "work");
         timeoutActivity.putExtra("work", Integer.parseInt(workVal.getText().toString()));
         timeoutActivity.putExtra("walk", Integer.parseInt(walkVal.getText().toString()));
 
         this.startActivity(timeoutActivity);
-        overridePendingTransition(0, 0);
+        overridePendingTransition(R.anim.pull_in_left, R.anim.push_out_right);
+    }
 
+    public boolean isValid(EditText input) {
+        if(input.getText().toString().isEmpty() || input.getText().toString().isEmpty()) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
